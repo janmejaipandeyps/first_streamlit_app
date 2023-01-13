2aee1ec8730633d59b9050504b4fc9509a7402e4
janmejaipandeyps <122527653+janmejaipandeyps@users.noreply.github.com>
2023-01-13 08:33:27 +0530

    fixed module

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-import reauests
+import reaqests
 fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/all")
 streamlit.text(fruityvice_response)
